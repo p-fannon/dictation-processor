45b5f65b9a451210d945c87f6fd578480362be0f
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-09-29 13:59:08 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DictationProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // iterate through subfolders of /mnt/uploads
+            // get metadata file
+            // extract metadata, including audio file info, from metadata file
+
+            // for each audio file listed in metadata:
+            // - get absolute file path
+            // - verify file checksum
+            // - generate a unique identifier
+            // - compress it
+            // - create a standalone metadata file 
+        }
+    }
+}
