67af89e5320e1870ade8f4a66c18e3e6341b0ef6
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-09-29 15:03:23 -0500

    Add metadata parsing feature

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace DictationProcessor
 {
@@ -8,7 +11,7 @@ namespace DictationProcessor
         static void Main(string[] args)
         {
             // iterate through subfolders of /mnt/uploads
-            foreach (var subfolder in Directory.GetDirectories("/mnt/uploads")) 
+            foreach (var subfolder in Directory.GetDirectories("/Users/fannonp/Documents/dotnet-core-mac-linux-getting-started/m3/demos/uploads")) 
             {
 
                 
@@ -16,7 +19,7 @@ namespace DictationProcessor
                 var metadataFilePath = Path.Combine(subfolder, "metadata.json");
                 Console.WriteLine($"Reading {metadataFilePath}");
                 // extract metadata, including audio file info, from metadata file
-                var metadataFileStream = File.Open(metadataFilePath, FileMode.Open);
+                var metadataCollection = GetMetadata(metadataFilePath);
 
                 // for each audio file listed in metadata:
                 // - get absolute file path
@@ -25,6 +28,17 @@ namespace DictationProcessor
                 // - compress it
                 // - create a standalone metadata file 
             }
+        }
+
+        static List<Metadata> GetMetadata(string metadataFilePath)
+        {
+            var metadataFileStream = File.Open(metadataFilePath, FileMode.Open);
+                var settings = new DataContractJsonSerializerSettings
+                {
+                    DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                };
+                var serializer = new DataContractJsonSerializer(typeof(List<Metadata>), settings);
+                return (List<Metadata>)serializer.ReadObject(metadataFileStream);
         }
     }
 }
