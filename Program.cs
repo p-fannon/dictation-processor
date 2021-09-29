15991bb55a14e110d82787e4042188d9b3f54465
Paul Fannon <fannonp@c02fm78vmd6m.wwt.com>
2021-09-29 15:43:59 -0500

    Add compression with metadata saving feature

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.IO.Compression;
 
 namespace DictationProcessor
 {
@@ -33,11 +34,28 @@ namespace DictationProcessor
                         throw new Exception("Checksum not verified! File corrupted?");
                     }
                     // - generate a unique identifier
+                    var uniqueId = Guid.NewGuid();
+                    metadata.File.FileName = uniqueId + ".WAV";
+                    var newPath = Path.Combine("/Users/fannonp/Documents/dotnet-core-mac-linux-getting-started/m3/demos/ready_for_transcription", uniqueId + ".WAV");
                     // - compress it
+                    CreateCompressedFile(audioFilePath, newPath);
                     // - create a standalone metadata file 
+                    SaveSingleMetadata(metadata, newPath + ".json");
                 }
                 
             }
+        }
+
+        static void CreateCompressedFile(string inputFilePath, string outputFilePath)
+        {
+            outputFilePath += ".gz";
+            Console.WriteLine($"Creating {outputFilePath}");
+
+            var inputFileStream = File.Open(inputFilePath, FileMode.Open);
+            var outputFileStream = File.Create(outputFilePath);
+            var gzipStream = new GZipStream(outputFileStream, CompressionLevel.Optimal);
+
+            inputFileStream.CopyTo(gzipStream);
         }
 
         static string GetChecksum(string filePath)
@@ -52,12 +70,24 @@ namespace DictationProcessor
         static List<Metadata> GetMetadata(string metadataFilePath)
         {
             var metadataFileStream = File.Open(metadataFilePath, FileMode.Open);
-                var settings = new DataContractJsonSerializerSettings
-                {
-                    DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-                };
-                var serializer = new DataContractJsonSerializer(typeof(List<Metadata>), settings);
-                return (List<Metadata>)serializer.ReadObject(metadataFileStream);
+            var settings = new DataContractJsonSerializerSettings
+            {
+                DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            };
+            var serializer = new DataContractJsonSerializer(typeof(List<Metadata>), settings);
+            return (List<Metadata>)serializer.ReadObject(metadataFileStream);
+        }
+
+        static void SaveSingleMetadata(Metadata metadata, string metadataFilePath)
+        {
+            Console.WriteLine($"Creating {metadataFilePath}");
+            var metadataFileStream = File.Open(metadataFilePath, FileMode.Create);
+            var settings = new DataContractJsonSerializerSettings
+            {
+                DateTimeFormat = new DateTimeFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            };
+            var serializer = new DataContractJsonSerializer(typeof(Metadata), settings);
+            serializer.WriteObject(metadataFileStream, metadata);
         }
     }
 }
